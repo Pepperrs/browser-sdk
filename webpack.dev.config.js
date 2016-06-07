ea4f9aeceb58ed393dd89946236439b8b9abb43f
Katie McGinley <katie@mcgnly.com>
2016-06-07 09:51:50 +0200

    config files that will change when we ditch karma and do everything in the browser

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,22 +2,22 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/main'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'relayr-browser-sdk.js', //bundle includes node_modules
-    publicPath: '/dist/'
-  },
-  plugins: [],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel']
-    }]
-  }
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/main'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'relayr-browser-sdk.js', //bundle includes node_modules
+        publicPath: '/dist/'
+    },
+    plugins: [],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel']
+        }]
+    }
 };

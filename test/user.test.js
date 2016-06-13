257a3604f2467d25e856f1c48da2fca73c0420b6
Katie McGinley <katie@mcgnly.com>
2016-06-13 10:47:37 +0200

    update tests

--- test/user.test.js
+++ test/user.test.js
@@ -59,4 +59,49 @@ describe('User', function() {
 
 
   });
+
+  describe('#getMyGroups', function() {
+
+
+    it('should get the current config', function() {
+      expect(userInstance._getConfig()).to.deep.equal(fakeConfig);
+    });
+
+    it('should resolve a promise with group info', function(done) {
+      let groupStub =
+        [{
+          "owner": "9210dce8-25da-4275-a73a-1383b9774255",
+          "position": 0,
+          "id": "4078de7d-1769-4078-92ec-ea08e2d57f54",
+          "name": "G0",
+          "devices": [{
+            "id": "1234dcedf-25da-427eda73a-1383b9774233",
+            "name": "my device",
+            "model": "MODELID",
+            "firmwareVersion": "2.0",
+            "secret": "123",
+            "owner": "9210dce8-25da-4275-a73a-1383b9774255",
+            "description": "desc",
+            "public": "false",
+            "externalId": "123",
+            "integrationType": "relayr",
+            "position": 0
+          }]
+        }];
+
+      userInstance.ajax.customXHR = this.xhr;
+      userInstance.getMyGroups().then((result) => {
+        expect(result).to.deep.equal(groupStub);
+        done();
+      });
+
+
+      this.requests[0].respond(200, {
+        'Content-Type': 'text/json'
+      }, JSON.stringify(userStub));
+    });
+
+
+
+  });
 });
